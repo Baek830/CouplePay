40c7a3b086232aad8b67892e709c03c897454e5b
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-09-30 23:17:45 +0900

    devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   root 'projects#index'
   get 'projects/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
