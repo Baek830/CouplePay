f810ff7529b7f298f9008fdedee9389e800d35ae
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-10-01 23:52:06 +0900

    logout function

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get "logout"
+    end
+  end
+
   root 'projects#index'
   get 'projects/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
