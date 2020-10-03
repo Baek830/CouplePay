ab1f0b1972ae11e8145014ee430b1fca03eaff48
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-10-03 12:04:59 +0900

    incomes controller updated

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,2 +1,14 @@
 class IncomesController < ApplicationController
+  def new
+    @income = Income.new
+  end
+  def create
+    @income = Income.new(income_params)
+    if @income.save
+      redirect_to user_path(id: current_user.id)
+    else
+      render :new
+    end
+  end
+
 end
