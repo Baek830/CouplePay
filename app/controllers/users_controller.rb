3f970e374233f2c1ee606173adc5183b7b5dedd8
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-10-07 22:26:34 +0900

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :find_user
+
   def show
-    @user = User.find(params[:id])
+    @incomes = @user.incomes
+    @outgoes = @user.outgoes
   end
   
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
