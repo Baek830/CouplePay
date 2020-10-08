e43560ac7f34398a840f762fdfe8c4085ee98b02
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-10-08 21:39:39 +0900

    income controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,19 @@ class UsersController < ApplicationController
   def show
     @incomes = @user.incomes
     @outgoes = @user.outgoes
+    @total_income = 0
+
   end
   
+  private
   def find_user
     @user = User.find(params[:id])
   end
+
+  def cal_income_total(income_values)
+    income_total = 0
+    income_values.each do |income_value|
+      income_total += 
+
+
 end
