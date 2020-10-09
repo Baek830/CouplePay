b1f7c037e504a249b1a16132157457fabb9fc6e9
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-10-09 23:42:13 +0900

    income total calc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def cal_income_total(income_values)
-    income_total = 0
-    income_values.each do |income_value|
-      income_total += 
 
 
 end
