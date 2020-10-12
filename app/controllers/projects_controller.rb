852568f41e5b5921943b4d3bf394f6fbfca13d77
Baek830 <66154033+Baek830@users.noreply.github.com>
2020-10-12 18:39:38 +0900

    updated scss

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,13 @@ class ProjectsController < ApplicationController
   def index
   end
 
+  def new
+    @project = Project.new(project_params)
+
   def show
   end
+
+  private
+  def project_params
+    params.require(:project).permit(:)
 end
